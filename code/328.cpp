8c12a1f1a5c661b362e25c90937a2d0f02dc5856
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2016-02-17 23:28:23 +0800

    add comment

--- code/328.cpp
+++ code/328.cpp
@@ -1,22 +1,30 @@
 
  // Definition for singly-linked list.
-
+/*
+[NULL] 边界条件
+[1]  边界条件
+[1,2,3,4] 正常case
+*/
 
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-
+        // 注意边界条件
         if(NULL==head || NULL==head->next||NULL==head->next->next) {
             return head;
         }
         ListNode * odd = head;
         ListNode * even = head->next;
+        // 需要保存两个链表的head
+        // first
+        // second
         ListNode * second = even;
         ListNode * first = odd;
 
         head = head->next->next;
    
-        int count = 3;
+        int count = 1;
+        //非常容易读的code
         while(head!=NULL) {
             if(count %2 ==1) {
                 odd->next = head;
@@ -24,7 +32,6 @@ public:
             } else {
                 even->next = head;
                 even = even->next;
-
             }
             head = head->next;
             count++;
