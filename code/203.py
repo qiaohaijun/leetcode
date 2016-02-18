ac9e22e9bb71028d7464ca3d3619807f316dec3d
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2016-02-18 21:29:57 +0800

    Create 203.py

---
+++ code/203.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        if head is None:
+            return
+        print head.val
+        dummy = ListNode(0)
+        dummy.next = head;
+        ret = dummy
+        
+        while dummy.next is not None:
+            if val == dummy.next.val:
+                dummy.next = dummy.next.next
+            else:
+                dummy = dummy.next
+        return ret.next
+                
