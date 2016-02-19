575e2af6e9fe27307bb499a4ed579300f2932d8e
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2016-02-19 12:51:18 +0800

    Create 2.py

---
+++ code/2.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# 位数是否相同
+# 倒序
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 is None and l2 is None:
+            return None
+        
+        dummy = ListNode(0)
+        retList = dummy
+        up = 0
+        while l1 is not None and l2 is not None:
+            # print (l1.val+l2.val+up)%10
+            dummy.next = ListNode((l1.val+l2.val+up)%10)
+            up = (l1.val+l2.val+up)/10
+            
+            # print "up",up
+            # print "l1.val", l1.val
+            # print "l2.val", l2.val
+            dummy = dummy.next
+            l1 = l1.next
+            l2 = l2.next
+            
+            
+        while l1 is not None:
+            dummy.next = ListNode((l1.val+up)%10)
+            up=(l1.val+up)/10
+            dummy = dummy.next
+            l1 = l1.next
+        while l2 is not None:
+            dummy.next = ListNode((l2.val+up)%10)
+            up=(l2.val+up)/10
+            dummy = dummy.next
+            l2 = l2.next
+            
+        if up>0:
+            dummy.next = ListNode(up)
+        return retList.next
